e06425d1fb3e8bd794558d1c51fe678f7cdb721c
Ninja-Cookie <noreply>
2025-06-18 18:04:14 +0100

    cleanup

--- TrueBRChaos/TwitchIntegration.cs
+++ TrueBRChaos/TwitchIntegration.cs
@@ -1,30 +1,4 @@
-﻿/*using TwitchLib.Api.Helix.Models.Polls.CreatePoll;
-using TwitchLib.Unity;
-
-namespace TrueBRChaos
-{
-    internal class TwitchIntegration
-    {
-        public void Init()
-        {
-            Api api = new Api();
-
-            api.Settings.ClientId = "zdx1ezslw56t22nng56jyezslgbzqo";
-            api.Settings.AccessToken = "bn4ix085exp2qafebyuqfnv4nbqzz1";
-
-            CreatePollRequest createPollRequest = new CreatePollRequest() { Title = "Test Poll" };
-            createPollRequest.Choices = new Choice[]
-            {
-                new Choice() { Title = "Option 1" },
-                new Choice() { Title = "Option 2" }
-            };
-
-            api.Helix.Polls.CreatePollAsync(createPollRequest);
-        }
-    }
-}*/
-
-namespace TrueBRChaos
+﻿namespace TrueBRChaos
 {
     internal class TwitchIntegration
     {
